2e31b4eca935e645c285803edc5d925f13deddc6
DaniilSkLi <danusha.lpdd@gmail.com>
2021-03-31 21:30:28 +0300

    fix: delete folder

--- MonoAdmin/index.php
+++ MonoAdmin/index.php
@@ -5,7 +5,7 @@ $index = true;
 require_once dirname(__DIR__) . "/Mono-root.php";
 
 if (file_exists(ROOT_PATH . "MonoInstall")) {
-    rmdir(ROOT_PATH . "MonoInstall");
+    deleteDirectory(ROOT_PATH . "MonoInstall");
 }
 
 require_once ROOT_PATH . "/MonoComponents/Core/core.php";
@@ -16,3 +16,28 @@ require_once ROOT_PATH . "MonoComponents/GetHead.php";
 require_once ROOT_PATH . "MonoComponents/kill.php";
 
 require_once __DIR__ . "/admin.php";
+
+
+
+function deleteDirectory($dir) {
+    if (!file_exists($dir)) {
+        return true;
+    }
+
+    if (!is_dir($dir)) {
+        return unlink($dir);
+    }
+
+    foreach (scandir($dir) as $item) {
+        if ($item == '.' || $item == '..') {
+            continue;
+        }
+
+        if (!deleteDirectory($dir . DIRECTORY_SEPARATOR . $item)) {
+            return false;
+        }
+
+    }
+
+    return rmdir($dir);
+}
